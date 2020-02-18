f459d086933df923c5d065198f3ccdaac50b59bf
Alison9 <al.franck@hotmail.com>
2020-02-18 16:06:18 +0100

    Ajout Layout

--- ParaglidingProject/Startup.cs
+++ ParaglidingProject/Startup.cs
@@ -43,10 +43,7 @@ namespace ParaglidingProject
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+               endpoints.MapDefaultControllerRoute();
             });
         }
     }
