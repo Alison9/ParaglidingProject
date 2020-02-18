a6e8c3dfb7e4d7a539b3402fa9c60fe4412abbe4
studentdot0407 <studentdot0407@TFNSDOTDE0407A>
2020-02-18 13:10:20 +0100

    pilote controller 1

---
+++ ParaglidingProject/Controllers/PilotsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ParaglidingProject.Data;
+using ParaglidingProject.Models;
+
+namespace ParaglidingProject.Controllers
+{
+    public class PilotsController : Controller
+    {
+        private readonly ParaglidingClubContext _context;
+
+        public PilotsController(ParaglidingClubContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pilots
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pilots.ToListAsync());
+        }
+
+        // GET: Pilots/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pilot = await _context.Pilots
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (pilot == null)
+            {
+                return NotFound();
+            }
+
+            return View(pilot);
+        }
+
+        // GET: Pilots/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pilots/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,FirstName,LastName,Adress,PhoneNumber,Weight,PostitionID,IsActif")] Pilot pilot)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pilot);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pilot);
+        }
+
+        // GET: Pilots/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pilot = await _context.Pilots.FindAsync(id);
+            if (pilot == null)
+            {
+                return NotFound();
+            }
+            return View(pilot);
+        }
+
+        // POST: Pilots/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,FirstName,LastName,Adress,PhoneNumber,Weight,PostitionID,IsActif")] Pilot pilot)
+        {
+            if (id != pilot.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pilot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PilotExists(pilot.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pilot);
+        }
+
+        // GET: Pilots/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pilot = await _context.Pilots
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (pilot == null)
+            {
+                return NotFound();
+            }
+
+            return View(pilot);
+        }
+
+        // POST: Pilots/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pilot = await _context.Pilots.FindAsync(id);
+            _context.Pilots.Remove(pilot);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PilotExists(int id)
+        {
+            return _context.Pilots.Any(e => e.ID == id);
+        }
+    }
+}
--- ParaglidingProject/Models/Subscription.cs
+++ ParaglidingProject/Models/Subscription.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace ParaglidingProject.Models
 {
     public class Subscription
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int YearID { get; set; }
         public decimal Price { get; set; }
