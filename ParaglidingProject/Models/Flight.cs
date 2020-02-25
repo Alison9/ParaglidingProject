71386b91202bfd63ea7eec97ccc9818fed0c0e77
studentdot0407 <studentdot0407@TFNSDOTDE0407A>
2020-02-25 16:45:55 +0100

    Changements sur models site et son CRUD

--- ParaglidingProject/Models/Flight.cs
+++ ParaglidingProject/Models/Flight.cs
@@ -10,6 +10,7 @@ namespace ParaglidingProject.Models
     {
         public int ID { get; set; }
 
+        [DataType(DataType.Time)]
         public DateTime FlightDate { get; set; }
 
         [DataType(DataType.Time)]
@@ -21,8 +22,11 @@ namespace ParaglidingProject.Models
         public int PilotID { get; set; }
         public int ParaglidingID { get; set; }
         public int SiteID { get; set; }
+        [Display(Name="Pilote")]
         public Pilot Pilot { get; set; }
+        [Display(Name= "Parapente")]
         public Paragliding Paragliding { get; set; }
+        [Display(Name="Site")]
         public Site Site { get; set; }
 
 
--- ParaglidingProject/Models/Site.cs
+++ ParaglidingProject/Models/Site.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,23 @@ namespace ParaglidingProject.Models
     public class Site
     {
         public int ID { get; set; }
+
+        [Display(Name="Nom")]
+        [Required]
+        [MaxLength(50 ,ErrorMessage="Le maximum de caractères est de 50")]
         public string Name { get; set; }
+        [Display(Name="Orientation pour l'atterrissage")]
         public string OrientationLanding{ get; set; }
+
+        [Display(Name="Orientation pour le décollage")]
         public string OrientationTakeOff { get; set; } // variable type spatial (longitude/latitude)
+
+        [Display(Name="Altitude au décollage")]
         public int? AltitudeTakeOff { get; set; }
+
+        [Display(Name="Type de vol")]
         public string FlightType { get; set; }
+
         public int LevelID { get; set; }
         public ICollection<Flight> Flights { get; set; }
         public Level Level { get; set; }
