8500034fe59007c30cd9d182b97fc46575fbafda
Maud Leleux <leleuxmaud@gmail.com>
2020-05-19 16:41:31 +0200

    Changements flight

--- ParaglidingProject.Data/ContextConfiguration/ModelsConfiguration/FlightConfiguration.cs
+++ ParaglidingProject.Data/ContextConfiguration/ModelsConfiguration/FlightConfiguration.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Paraglider.DAL.Models;
+using ParaglidingProject.Entities;
+using ParaglidingProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,27 +12,27 @@ namespace Paraglider.DAL.ContextConfiguration.ModelsConfiguration
     {
         public void Configure(EntityTypeBuilder<Flight> builder)
         {
-            builder.HasQueryFilter(p => p.IsActive);
+          // builder.HasQueryFilter(p => p. IsActive);
 
             builder.HasOne(p => p.Paraglider)
                 .WithMany(fs => fs.Flights)
-                .HasForeignKey(k => k.ParagliderId)
+                .HasForeignKey(k => k.ParagliderID)
                 .IsRequired(true)
                 .OnDelete(DeleteBehavior.Restrict);
             builder.HasOne(p => p.Pilot)
                 .WithMany(fs => fs.Flights)
-                .HasForeignKey(k => k.PilotId)
+                .HasForeignKey(k => k.PilotID)
                 .IsRequired(true)
                 .OnDelete(DeleteBehavior.Restrict);
             builder.HasOne(s => s.Site)
                 .WithMany(fs => fs.Flights)
-                .HasForeignKey(k => k.SiteId)
+                .HasForeignKey(k => k.SiteID)
                 .IsRequired(true)
                 .OnDelete(DeleteBehavior.Restrict);
 
             builder.Property(sc => sc.FlightDate)
                 .HasColumnType("date");
-            builder.Property(sc => sc.FlightDuration)
+            builder.Property(sc => sc.Duration)
                 .HasColumnType("decimal(5,2)");
         }
     }
