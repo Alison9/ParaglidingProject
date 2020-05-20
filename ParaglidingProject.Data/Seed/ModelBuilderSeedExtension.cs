f5ab19c489c7d4ea572e8556df89b1b0a20f343b
Alison Franck <al.franck@hotmail.com>
2020-05-20 14:42:24 +0200

    Seed Created

---
+++ ParaglidingProject.Data/Seed/ModelBuilderSeedExtension.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaglidingProject.Data.Seed
+{
+    public static class ModelBuilderSeedExtension
+    {
+        public static void PilotSeed(this ModelBuilder modelBuilder)
+        {
+            //Abdel
+        }
+
+        public static void FlightSeed(this ModelBuilder modelBuilder)
+        {
+            //Abdel
+        }
+
+        public static void LevelSeed(this ModelBuilder modelBuilder)
+        {
+            //Yves
+        }
+
+        public static void LicenseSeed(this ModelBuilder modelBuilder)
+        {
+            //Alison
+        }
+
+        public static void ParagliderSeed(this ModelBuilder modelBuilder)
+        {
+            //Francisco
+        }
+
+        public static void ParagliderModelSeed(this ModelBuilder modelBuilder)
+        {
+            //Steve
+        }
+
+        public static void PilotTraineeshipSeed(this ModelBuilder modelBuilder)
+        {
+            //Maud
+        }
+
+        public static void PossessionSeed(this ModelBuilder modelBuilder)
+        {
+            //Ruaa
+        }
+
+        public static void RoleSeed(this ModelBuilder modelBuilder)
+        {
+            //Lionel
+        }
+
+        public static void SiteSeed(this ModelBuilder modelBuilder)
+        {
+            //Alison
+        }
+
+        public static void SubscriptionSeed(this ModelBuilder modelBuilder)
+        {
+            //Lionel
+        }
+
+        public static void SubscriptionPaymentSeed(this ModelBuilder modelBuilder)
+        {
+            //Francisco
+        }
+
+        public static void TraineeshipSeed(this ModelBuilder modelBuilder)
+        {
+            //Steve
+        }
+
+        public static void TraineeshipPaymentSeed(this ModelBuilder modelBuilder)
+        {
+            //Maud
+        }
+
+
+
+    }
+}
