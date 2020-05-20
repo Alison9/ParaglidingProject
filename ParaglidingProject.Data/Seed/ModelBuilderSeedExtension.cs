a1e3deeb02ffeb2fc3f59cccf8b814c95f8f9b3f
yves blavier <escaflowne99@msn.com>
2020-05-20 15:27:44 +0200

    levelSeed creation

--- ParaglidingProject.Data/Seed/ModelBuilderSeedExtension.cs
+++ ParaglidingProject.Data/Seed/ModelBuilderSeedExtension.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ParaglidingProject.Models;
 
 namespace ParaglidingProject.Data.Seed
 {
@@ -20,6 +21,40 @@ namespace ParaglidingProject.Data.Seed
         public static void LevelSeed(this ModelBuilder modelBuilder)
         {
             //Yves
+            modelBuilder.Entity<Level>().HasData(
+                new Level
+                {
+                    LevelID = 1,
+                    Name = "Level 1",
+                    Skill = "brevet A",
+                    IsActive = true
+                },
+                new Level
+                   { LevelID = 2,
+                    Name = "Level 2",
+                    Skill = "brevet B",
+                    IsActive = true
+                },
+                new Level
+                {
+                    LevelID = 3,
+                    Name = "Level 3",
+                    Skill = "brevet C",
+                    IsActive = true
+
+                },
+                new Level
+                {
+                    LevelID = 4,
+                    Name = "Level 4",
+                    Skill = "brevet D",
+                    IsActive = false
+
+                }
+             );
+
+        }
+    }
         }
 
         public static void LicenseSeed(this ModelBuilder modelBuilder)
