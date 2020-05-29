7d6ca4f12779c3306950b6a607f57265e668733a
fjcarmo <franciscojccarmo@gmail.com>
2020-05-29 15:49:54 +0200

    LevelService + LevelController

---
+++ ParaglidingProject.API/Controllers/LevelsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParaglidingProject.SL.Core.Levels.NS;
+using ParaglidingProject.SL.Core.Levels.NS.TransfertObjects;
+
+namespace ParaglidingProject.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1/levels/")]
+    public class LevelsController : ControllerBase
+    {
+        private readonly ILevelsService _levelsService;
+
+        public LevelsController(ILevelsService levelsService)
+        {
+            _levelsService = levelsService ?? throw new ArgumentNullException(nameof(levelsService));
+        }
+
+        [HttpGet("{levelId}", Name = "GetLevelAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<LevelDto>> GetLevelAsync([FromRoute] int levelId)
+        {
+            var level = await _levelsService.GetLevelAsync(levelId);
+            if (level == null) return NotFound("Couldn't find any associated Level");
+            return Ok(level);
+        }
+
+        [HttpGet("", Name = "GetAllLevelsAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IReadOnlyCollection<LevelDto>>> GetAllLevelsAsync()
+        {
+            var levels = await _levelsService.GetAllLevelsAsync();
+            if (levels == null) return NotFound("Collection was empty :O");
+            return Ok(levels);
+        }
+    }
+}
---
+++ ParaglidingProject.SL.Core/Levels.NS/LevelsService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParaglidingProject.Data;
+using ParaglidingProject.SL.Core.Levels.NS.TransfertObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParaglidingProject.SL.Core.Levels.NS
+{
+    public class LevelsService : ILevelsService
+    {
+        private readonly ParaglidingClubContext _paraContext;
+
+        public LevelsService(ParaglidingClubContext paraContext)
+        {
+            _paraContext = paraContext ?? throw new ArgumentOutOfRangeException(nameof(paraContext));
+        }
+        
+        public async Task<LevelDto> GetLevelAsync(int id)
+        {
+            var level = await _paraContext.Levels
+                .AsNoTracking()
+                .Select(l => new LevelDto
+                {
+                    LevelID = l.ID,
+                    Name = l.Name,
+                    Skill = l.Skill,
+                    IsActive = l.IsActive
+                })
+                .FirstOrDefaultAsync(l => l.LevelID == id);
+
+            return level;
+        }
+        public async Task<IReadOnlyCollection<LevelDto>> GetAllLevelsAsync()
+        {
+            var levels = _paraContext.Levels
+                .AsNoTracking()
+                .Select(l => new LevelDto
+                {
+                    LevelID = l.ID,
+                    Name = l.Name,
+                    Skill = l.Skill,
+                    IsActive = l.IsActive
+                });
+
+            return await levels.ToListAsync();
+        }
+    }
+}
