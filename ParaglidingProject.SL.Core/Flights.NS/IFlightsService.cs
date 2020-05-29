a95fa6035bae6e47a565c9493b8441ab02c8129b
Alison Franck <al.franck@hotmail.com>
2020-05-29 15:58:03 +0200

    Ajout de Flight.NS (service + Dto) + FlightsController

---
+++ ParaglidingProject.SL.Core/Flights.NS/IFlightsService.cs
@@ -0,0 +1,14 @@
+﻿using ParaglidingProject.SL.Core.Flights.NS.TransfertObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaglidingProject.SL.Core.Flights.NS
+{
+    public interface IFlightsService
+    {
+        Task<FlightDto> GetFlightAsync(int id);
+        Task<IReadOnlyCollection<FlightDto>> GetAllFlightsAsync();
+    }
+}
---
+++ ParaglidingProject.SL.Core/Flights.NS/MapperProfiles/FlightMapping.cs
@@ -0,0 +1,26 @@
+﻿using ParaglidingProject.Models;
+using ParaglidingProject.SL.Core.Flights.NS.TransfertObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParaglidingProject.SL.Core.Flights.NS.MapperProfiles
+{
+    public static class FlightMapping
+    {
+        public static FlightDto MapFlightDto(this Flight flight)
+        {
+            var flightDto = new FlightDto
+            {
+                FlightId = flight.ID,
+                FlightDate = flight.FlightDate,
+                Duration = flight.Duration,
+                PilotName = $"{flight.Pilot.FirstName} {flight.Pilot.LastName}",
+                ParagliderName = flight.Paraglider.Name,
+                TakeOffSiteName = flight.TakeOffSite.Name,
+                LandingSiteName = flight.LandingSite.Name
+            };
+            return flightDto;
+        }
+    }
+}
