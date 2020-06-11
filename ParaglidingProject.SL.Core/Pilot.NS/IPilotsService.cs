133d6a9e14158b755e2eb1b2c121673064da6cc0
ruaaa1982alagha <ruaaa1982alagha@gmail.com>
2020-06-11 14:20:04 +0200

    error

--- ParaglidingProject.SL.Core/Pilot.NS/IPilotsService.cs
+++ ParaglidingProject.SL.Core/Pilot.NS/IPilotsService.cs
@@ -3,23 +3,14 @@ using System.Threading.Tasks;
 using ParaglidingProject.SL.Core.Pilot.NS.TransfertObjects;
 
 namespace ParaglidingProject.SL.Core.Pilot.NS
-{
-    /// <summary>
-    /// Pilot's contract to get a list of pilots or a specific pilot.
-    /// </summary>
+{ 
+   
     public interface IPilotsService
     {
-        /// <summary>
-        /// Obtain bussiness-related informations of a pilot by searching their id.
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns>Pilot Dto containing pilot id, name, address and number of flights</returns>
+       
         Task<PilotDto> GetPilotAsync(int id);
-
-        /// <summary>
-        /// Obtain bussiness-related informations of a list of pilots.
-        /// </summary>
-        /// <returns>Collection of Pilot Dto</returns>
+        
+       
         Task<IReadOnlyCollection<PilotDto>> GetAllPilotsAsync();
     }
 }
