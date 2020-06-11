a5238078493af088ca45ccd010704a2fe885c5bf
Alison Franck <al.franck@hotmail.com>
2020-06-11 12:08:05 +0200

    :sparkles: Documentation IParagliderModelsService

--- ParaglidingProject.SL.Core/ParagliderModel.NS/IParagliderModelService.cs
+++ ParaglidingProject.SL.Core/ParagliderModel.NS/IParagliderModelService.cs
@@ -4,9 +4,22 @@ using System.Threading.Tasks;
 
 namespace ParaglidingProject.SL.Core.ParagliderModel.NS
 {
+    /// <summary>
+    /// Paraglider model's contract
+    /// </summary>
   public interface IParagliderModelService
   {
+        /// <summary>
+        /// Get a paraglider model
+        /// </summary>
+        /// <param name="ID">Paraglider model's ID</param>
+        /// <returns> A paraglider model</returns>
     Task<ParagliderModelDto> GetParagliderModelAsync(int ID);
+
+        /// <summary>
+        /// Get all paraglider models
+        /// </summary>
+        /// <returns>A collection of paraglider models</returns>
     Task<IReadOnlyCollection<ParagliderModelDto>> GetAllParagliderModelsAsync();
   }
 }
