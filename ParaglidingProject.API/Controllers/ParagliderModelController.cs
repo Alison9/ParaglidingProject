535a96cd9b0c27fd9ba3c5e1d60c594e97e8561d
Nga Nguyen <dahi1988@yahoo.com>
2020-06-11 16:22:53 +0200

    :bulb: add comment on paraglidermodelscontroller

--- ParaglidingProject.API/Controllers/ParagliderModelController.cs
+++ ParaglidingProject.API/Controllers/ParagliderModelController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ParaglidingProject.SL.Core.ParagliderModel.NS;
@@ -9,10 +10,13 @@ using ParaglidingProject.SL.Core.ParagliderModel.NS.TransfertObjects;
 
 namespace ParaglidingProject.API.Controllers
 {
+    /// <summary>
+    /// the controller of paragliderModels
+    /// </summary>
     [ApiExplorerSettings(GroupName = "paragliderModels")]
     [Route("api/v1/[controller]")]
-  [Produces("application/json")]
-  [ApiController]
+    [Produces("application/json")]
+    [ApiController]
     public class ParagliderModelController : ControllerBase
     {
     private readonly IParagliderModelService _ModelParagliderService;
@@ -21,19 +25,37 @@ namespace ParaglidingProject.API.Controllers
     {
       _ModelParagliderService = modelparagliderService ?? throw new ArgumentNullException(nameof(modelparagliderService));
     }
-    [HttpGet("{modelParagliderId}")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<ActionResult<ParagliderModelDto>> GetParagliderModelAsync([FromRoute] int modelParagliderId)
+
+        /// <summary>
+        /// get a paraglider by id
+        /// </summary>
+        /// <param name="paragliderModelId">The unique id of a paraglider</param>
+        /// <returns>
+        /// an actionresult of type 202 who contain a paragliderDto
+        /// an actionresult of type 404 if no paraglider was find
+        /// <seealso cref="ParagliderModelDto"/>
+        /// </returns>
+        /// <remarks></remarks>
+
+        [HttpGet("{paragliderModelId}", Name = "GetParagliderModelAsync")]
+
+        public async Task<ActionResult<ParagliderModelDto>> GetParagliderModelAsync([FromRoute] int modelParagliderId)
     {
       var modelParaglider = await _ModelParagliderService.GetParagliderModelAsync(modelParagliderId);
       if (modelParaglider == null) return NotFound("Couldn't find any model of paraglider");
       return Ok(modelParaglider);
     }
+        /// <summary>
+        /// get all paraglider
+        /// </summary>
+        /// <returns>
+        /// an actionresult of type 202 who contain a list of paragliderModelDto
+        /// an actionresult of type 404 if no paraglidermodel was find
+        /// <seealso cref="ParagliderModelDto"/>
+        /// </returns>
 
-    [HttpGet]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("", Name = "GetAllModelParaglidersAsync")]
+        
     public async Task<ActionResult<IReadOnlyCollection< ParagliderModelDto>>> GetAllModelParaglidersAsync()
     {
       var listModelParaglider = await _ModelParagliderService.GetAllParagliderModelsAsync();
