f3ca308387a424c63d534bd7479e34f656c9b887
Maud Leleux <leleuxmaud@gmail.com>
2020-06-11 16:19:28 +0200

    :bulb: Irole :bulb: AuthController

--- ParaglidingProject.API/Controllers/AuthController.cs
+++ ParaglidingProject.API/Controllers/AuthController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +11,14 @@ using ParaglidingProject.SL.Core.Auth.NS.TransfertObjects;
 
 namespace ParaglidingProject.API.Controllers
 {
+        /// <summary>
+        /// Return a Token for an authorized member. Asynchronous ActionResult
+        /// </summary>
+        /// <param name="credentials">Informations for verification</param>   
+        /// <response code="200">Member authorized</response>
+        /// <response code="401">Person unauthorized</response>
+		    /// <response code="404">Person not found</response>
+			  /// seealso cref="authDto" 
     [Authorize]
     [Route("api/v1/auth")]
     [ApiExplorerSettings(GroupName ="authentification")]
