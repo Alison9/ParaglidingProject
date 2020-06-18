4aaa4886dc25a1478b18d15ff67350df674f0e15
Abdel <abdel.ali.hnini@hotmail.com>
2020-06-18 12:52:06 +0200

    *sparkles* TraineeshpPayementSSFP

--- ParaglidingProject.SL.Core/TraineeshipPayment.NS/TransferObjects/TraineeshipPayementSSFP.cs
+++ ParaglidingProject.SL.Core/TraineeshipPayment.NS/TransferObjects/TraineeshipPayementSSFP.cs
@@ -3,41 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ParaglidingProject.SL.Core.Flights.NS.Helpers
+namespace ParaglidingProject.SL.Core.TraineeshipPayment.NS.TransferObjects
 {
-    class FlightsSSFP
+    class TraineeshipPayementSSFP
     {
         private const int DefaultPageSize = 1;
         private const int MaxPageSize = 10;
-        public int PageNumber { get; set; } = 1;
         private int _pageSize = DefaultPageSize;
+        public int PageNumber { get; set; } = 1;
         public int PageSize
         {
             get => _pageSize;
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
-        public int TotalPages { get; private set; }
+        public int TotalePages { get; private set; }
         public int TotalCount { get; private set; }
-        public void SetPagingValues<T> (IQueryable<T> query)
+        public void SetPagingValue<T>(IQueryable<T> query)
         {
             TotalCount = query.Count();
+            TotalPage = TotalCount / PageSize;
 
-            TotalPages = TotalCount / PageSize;
-
-            if((double)TotalCount/PageSize != 0)
+            if ((double)TotalCount / PageSize != 0)
             {
-                TotalPages++;
+                TotalPage++;
             }
 
             if (PageNumber < 1)
                 PageNumber = 1;
 
-            if (PageNumber > TotalPages)
-                PageNumber = TotalPages;
+            if (PageNumber > TotalPage)
+                PageNumber = TotalPage;
 
             if (PageNumber < 0)
                 PageNumber = 1;
-                
         }
+
+
     }
 }
