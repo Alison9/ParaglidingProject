690ef9d8c4197fa1e37451710c0785e4708f8330
fjcarmo <franciscojccarmo@gmail.com>
2020-06-19 09:19:19 +0200

    :sparkles: ParagliderSSFP

--- ParaglidingProject.API/Controllers/ParagliderController.cs
+++ ParaglidingProject.API/Controllers/ParagliderController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Options;
+using System.Text.Json;
 using ParaglidingProject.SL.Core.Paraglider.NS;
+using ParaglidingProject.SL.Core.Paraglider.NS.Helpers;
 using ParaglidingProject.SL.Core.Paraglider.NS.TransfertObjects;
 
 namespace ParaglidingProject.API.Controllers
@@ -51,6 +54,7 @@ namespace ParaglidingProject.API.Controllers
         /// <summary>
         /// get all paraglider
         /// </summary>
+        /// <param name="options">User options to search, sort, filter and paginate a list of paragliders obtained from a query strings.</param>
         /// <returns>
         /// an actionresult of type 202 who contain a list of paragliderDto
         /// an actionresult of type 404 if no paraglider was find
@@ -58,11 +62,69 @@ namespace ParaglidingProject.API.Controllers
         /// </returns>
         [HttpGet("", Name = "GetAllParaglidersAsync")]
      
-        public async Task<ActionResult<IReadOnlyCollection<ParagliderDto>>> GetAllParaglidersAsync()
+        public async Task<ActionResult<IReadOnlyCollection<ParagliderDto>>> GetAllParaglidersAsync([FromQuery] ParaglidersSSFP options)
         {
-            var paraglider = await _paragliderService.GetAllParaglidersAsync();
+            var paraglider = await _paragliderService.GetAllParaglidersAsync(options);
             if (paraglider == null) return NotFound("There is no paraglider ");
+
+            var previousPageLink = options.HasPrevious ? CreateResourceUri(options, ResourceUriType.PreviousPage) : null;
+            var nextPageLink = options.HasNext ? CreateResourceUri(options, ResourceUriType.NextPage) : null;
+
+            var paginationMetadata = new
+            {
+                options.TotalCount,
+                options.PageSize,
+                options.PageNumber,
+                options.TotalPages,
+                previousPageLink,
+                nextPageLink
+            };
+
+            Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(paginationMetadata));
+
             return Ok(paraglider);
+        }
+
+        /// <summary>
+        /// Refactoring method that, given user parameters, creates a custom URL link to the previous and next page.
+        /// </summary>
+        /// <param name="options">The user custom options or default options for the search, sort, filter and pagination features.</param>
+        /// <param name="type">One element of the enumeration to distinguish the multiple types of URLs to create.</param>
+        /// <returns>
+        /// A string that contains an URL link to the previous, the next page, or the current page by default.
+        /// </returns>
+        private string CreateResourceUri(ParaglidersSSFP options, ResourceUriType type)
+        {
+            switch (type)
+            {
+                case ResourceUriType.PreviousPage:
+                    return Url.Link("GetAllParaglidersAsync",
+                        new
+                        {
+                            PageNumber = options.PageNumber = 1,
+                            options.PageSize
+                        });
+                case ResourceUriType.NextPage:
+                    return Url.Link("GetAllParaglidersAsync",
+                        new
+                        {
+                            PageNumber = options.PageNumber + 1,
+                            options.PageSize
+                        });
+                default:
+                    return Url.Link("GetAllParaglidersAsync",
+                        new
+                        {
+                            options.PageNumber,
+                            options.PageSize
+                        });
+            }
+        }
+
+        public enum ResourceUriType
+        {
+            PreviousPage = 0,
+            NextPage = 1
         }
     }
 }
